f692af7cb8361c84ad489a76f8e6a43c91897c06
Tadgh Purcell <tadghp@gmail.com>
2023-01-21 11:17:57 +0000

    Fix issue of dividing grid into equal squares

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ sketchpad.appendChild(container);
 container.classList.add('screen');
 
 //grid loop
-let playerInput = 16;
+let playerInput = 16 * 16;
 while (playerInput > 0) {
   playerInput--;
   const grid = document.createElement('div');
   grid.style.borderStyle = 'solid';
   grid.style.borderColor = 'black';
   grid.style.borderWidth = '1px';
-  grid.textContent = playerInput;
+  grid.classList.add('square');
   container.appendChild(grid);
 }
