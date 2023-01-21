e2c8c879b93c426c43554eef6ab8537bd800fd1a
Tadgh Purcell <tadghp@gmail.com>
2023-01-21 12:21:43 +0000

    Add buttons for 8x8 and 16x16

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
 'use strict';
 const container = document.createElement('div');
 const sketchpad = document.querySelector('.sketchpad');
+const btnColor = document.querySelector('.color');
+const btnEraser = document.querySelector('.eraser');
+const btn16 = document.querySelector('.sixteen');
+const btn8 = document.querySelector('.eight');
+let playerInput;
 sketchpad.appendChild(container);
 container.classList.add('screen');
 
+btnColor.addEventListener('click', function () {
+  btnColor.classList.add('active');
+  btnEraser.classList.remove('active');
+});
+
+btnEraser.addEventListener('click', function () {
+  btnColor.classList.remove('active');
+  btnEraser.classList.add('active');
+});
+
+btn16.addEventListener('click', function () {
+  playerInput = 16 * 16;
+  while (playerInput > 0) {
+    playerInput--;
+    const grid = document.createElement('div');
+    grid.style.borderStyle = 'solid';
+    grid.style.borderColor = 'black';
+    grid.style.borderWidth = '1px';
+    grid.classList.add('square');
+
+    grid.addEventListener('mouseover', function () {
+      grid.classList.add('sqcolor');
+      if (btnColor.classList.contains('active')) {
+        grid.classList.add('sqcolor');
+      } else {
+        grid.classList.remove('sqcolor');
+      }
+    });
+    container.appendChild(grid);
+  }
+});
+
+btn8.addEventListener('click', function () {
+  playerInput = 8 * 8;
+  while (playerInput > 0) {
+    playerInput--;
+    const grid = document.createElement('div');
+    grid.style.borderStyle = 'solid';
+    grid.style.borderColor = 'black';
+    grid.style.borderWidth = '1px';
+    grid.classList.add('square2');
+
+    grid.addEventListener('mouseover', function () {
+      grid.classList.add('sqcolor');
+      if (btnColor.classList.contains('active')) {
+        grid.classList.add('sqcolor');
+      } else {
+        grid.classList.remove('sqcolor');
+      }
+    });
+    container.appendChild(grid);
+  }
+});
 //grid loop
-let playerInput = 16 * 16;
+// let playerInput = 16 * 16;
 while (playerInput > 0) {
   playerInput--;
   const grid = document.createElement('div');
@@ -15,7 +73,19 @@ while (playerInput > 0) {
   grid.classList.add('square');
 
   grid.addEventListener('mouseover', function () {
-    grid.classList.toggle('sqcolor');
+    grid.classList.add('sqcolor');
+    if (btnColor.classList.contains('active')) {
+      grid.classList.add('sqcolor');
+    } else {
+      grid.classList.remove('sqcolor');
+    }
   });
   container.appendChild(grid);
 }
+
+// grid.addEventListener('mouseover', function () {
+//   grid.classList.add('sqcolor');
+// });
+
+// square.textContent = 'hello';
+// grid.textContent = 'hello';
