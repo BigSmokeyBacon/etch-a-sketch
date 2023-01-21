565ac737191bb7a02065b0447c72f69c65a7d160
Tadgh Purcell <tadghp@gmail.com>
2023-01-21 10:14:14 +0000

    Create js container element for sketchpad

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+const container = document.createElement('div');
+const sketchpad = document.querySelector('.sketchpad');
+container.textContent = 'Hello Im in';
+sketchpad.appendChild(container);
